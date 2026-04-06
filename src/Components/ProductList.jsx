424459676a71fc74b867703b250965ceffacc11b
Sridhar-cho <sridharmech432@gmail.com>
2026-04-06 16:25:19 +0530

    final changes

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -13,6 +13,9 @@ const ProductList = () => {
 
     const dispatch = useDispatch();
     const cartItems =useSelector(state => state.cart.cartItems);
+    const handleAddToCart =(product)=>{
+        dispatch(addItemToCart(product))
+    }
     return (
         <div className="product-list">
             <h2 className="product-list-title">Products</h2>
@@ -20,8 +23,12 @@ const ProductList = () => {
                 {products.map(product => (
                     <li key={product.id} className="product-list-item">
                         <span>{product.name} - ${product.price}</span>
-                        <button>
-                            Add to Cart
+                        <button
+                            className={`add-to-cart-btn ${cartItems.some(item => item.id === product.id) ? "disabled" : ""}`}
+                            onClick={()=> handleAddToCart(product)}
+                            disabled={cartItems.some(item => item.id === product.id)}
+                        >
+                            {cartItems.some(item => item.id === product.id) ? "Added" : "Add to Cart"}
                         </button>
                     </li>
                 ))}
--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
 import './ShoppingCart.css'; 
+import { useSelector, useDispatch } from 'react-redux';
+import { clearCart, increaseItemQuantity, decreaseItemQuantity, removeItemFromCart } from './CartSlice';
 
 const ShoppingCart = () => {
-
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart.cartItems);
+  const totalAmount = cartItems.reduce((total, cartItem)=> total + cartItem.price * cartItem.quantity, 0)
+  const handleClearCart = ()=>{
+    dispatch(clearCart())
+  }
+  const handleIncreaseItem =(productId)=>{
+    dispatch(increaseItemQuantity(productId))
+  }
+  const handleDecreaseItem = (productId)=>{
+    dispatch(decreaseItemQuantity(productId))
+  }
+  const handleRemoveItem =(productId)=>{
+    dispatch(removeItemFromCart(productId))
+  }
   return (
     <>
     <div className="shopping-cart">
       <h2 className="shopping-cart-title">Shopping Cart</h2>
       <ul className="cart-items">
-       
+       {cartItems.map(item=> (
+        <li key={item.id} className="cart-item">
+          <span>{item.name} - ${item.price}</span>
+          <div className='quantity-controls'>
+            <button onClick={()=>handleDecreaseItem(item.id)}> - </button>
+            <span>{item.quantity}</span>
+            <button onClick={()=>handleIncreaseItem(item.id)}> + </button>
+          </div>
+          <button className='remove-item-btn' onClick={()=> handleRemoveItem(item.id)}>Remove</button>
+        </li>
+       ))}
       </ul>
-      <button className="clear-cart-btn">Clear Cart</button>
+      <button 
+      className={`clear-cart-btn ${cartItems.length === 0 ? "disabled":""}`}
+      disabled={cartItems.length === 0}
+      onClick={()=>handleClearCart()}
+      >
+        Clear Cart
+      </button>
     </div>
+    <div className='total-amount'>{totalAmount ? <h1>The total amount is ${totalAmount}</h1> : ""}</div>
   
     </>
   );
